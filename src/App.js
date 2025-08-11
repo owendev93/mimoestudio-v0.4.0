998b157b7b3e678cbfb96e7fefa4c989492939f3
owendev93 <owenpuertasauto@gmail.com>
2025-08-11 18:44:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HeroSection from './components/HeroSection';
 import GallerySection from './components/GallerySection';
 import AboutSection from './components/AboutSection'; // Importar la nueva sección
 import ContactSection from './components/ContactSection';
+import { User } from 'lucide-react'; // Agrega esta línea arriba
 
 const App = () => {
   return (
@@ -15,8 +16,12 @@ const App = () => {
         <AboutSection /> {/* Añadir la nueva sección aquí */}
         <ContactSection />
       </main>
-      <footer className="bg-purple-900 text-white py-8 text-center">
+      <footer className="bg-purple-900 text-white py-8 text-center flex justify-center items-center gap-x-[80rem]">
         <p>&copy; {new Date().getFullYear()} MiMO Estudio. Todos los derechos reservados.</p>
+        <p>
+        <User className="inline w-5 h-5 mr-2" />
+        Desarrollado por: Owen Puerta Sauto.
+        </p>
       </footer>
     </div>
   );
