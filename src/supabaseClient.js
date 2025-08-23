8a42a40010da0fabb3c79ac6b9606be157f9cccd
owendev93 <owenpuertasauto@gmail.com>
2025-08-23 20:15:16 -0300

    Update Section Contact

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://gcudhrrrizgrmuuzrmht.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjdWRocnJyaXpncm11dXpybWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0NzAwNTUsImV4cCI6MjA3MTA0NjA1NX0.9e952txyAO4yFdGN1IvtdFzLSleXxU22w6JXe_M-JO8';
+const supabaseUrl = 'https://gcudhrrrizgrmuuzrmht.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjdWRocnJyaXpncm11dXpybWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0NzAwNTUsImV4cCI6MjA3MTA0NjA1NX0.9e952txyAO4yFdGN1IvtdFzLSleXxU22w6JXe_M-JO8'; // Usa la clave pública (anon)
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
